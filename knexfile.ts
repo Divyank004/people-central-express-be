8833089141f9164fabcbd077da19c007c5e08648
Divyank004 <divyank004@gmail.com>
2025-08-12 15:51:49 +0200

    update docker compose command to run initial db seeding

--- knexfile.ts
+++ knexfile.ts
@@ -47,7 +47,7 @@ module.exports = {
       port: process.env.DB_PROD_PORT,
       database: process.env.DB_PROD_DBNAME,
       user: process.env.DB_PROD_USER,
-      password: process.env.POSTGRES_PASSWORD
+      password: process.env.DB_PROD_PASS
     },
     pool: {
       min: 2,
